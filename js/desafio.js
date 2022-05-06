7616caa07630312f2687124cc98c4b19659ed86d
NicolasMoraesDeSouza <seilanicolas05@gmail.com>
2022-05-06 15:38:53 -0300

    Concluindo desafio 2 e começando desafio 3

--- js/desafio.js
+++ js/desafio.js
@@ -10,18 +10,16 @@ function reverseNum(num) {
   )
 }
 
-function mostraResultado(){
+function showResult(){
 
   for (let i = 10; i < 1000000;  i++ ) {
     a = reverseNum(i)
     b = a + i
     mod = b % 2
-    if(mod != 0){
-      
+    if(mod != 0){ 
       
       console.log(b)
 
-  
     }
     else{
       
@@ -29,7 +27,38 @@ function mostraResultado(){
   }
         
 }
+function challenge2(){
+  parametro1 = document.getElementById("studentTime").value
+  parametro2 = document.getElementById("studentLimit").value
+  a = parametro1.split(",").map(value => parseInt(value))
+  b = a.length
+    
+    lateStudents = a.filter(function e(value){
+     
+       return value > 0 
+     
+    })
 
+  c = lateStudents.length
+
+  if ( c  >= parametro2 ){
+    console.log("Aula cancelada")
+
+  }
+  else {
+    console.log("Aula normal")
+  }
+}
+function challenge3 (){
+  numeroX = document.getElementById("N").value
+  arrX = document.getElementById("vetorSoma").value
+  arr = arrX.split(",").map(value => parseInt(value))
+  orderedHypothesis = arr.order(arr => Math.abs(arr.result - numeroX))
+ 
+
+
+
+}
 
     
     
